413be70bdaf81d2b35c2e505c4e4feb43f17c81f
Chris <christopher.loggins@gmail.com>
2021-03-12 14:55:03 -0600

    Various project setup.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <Paper>
+                        <div>Hello, world.</div>
+                        <div>learn react</div>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </div>
+    )
 }
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import renderer from 'react-test-renderer'
+import App from './App'
+
+test('renders correctly', () => {
+    const tree = renderer.create(<App />).toJSON()
+
+    expect(tree).toMatchSnapshot()
+})
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+    render(<App />)
+    const linkElement = screen.getByText(/learn react/i)
+    expect(linkElement).toBeInTheDocument()
+})
